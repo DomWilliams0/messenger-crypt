79a57d73d260c9407f88e9cfc9345b4ad05bd0ff
Dom Williams <me@domwillia.ms>
2016-10-01 23:35:04 +0200

    Enhance reusability and synergy in the workplace

--- crypto-server/encryption.py
+++ crypto-server/encryption.py
@@ -40,15 +40,9 @@ def decrypt_message(msg):
     decrypt_key_id = config['keys.decryption']
 
     # find decryption key
-    decrypt_keys = list(GPGContext.INSTANCE.keylist(decrypt_key_id, True))
-    if not decrypt_keys:
-        msg.error = "Secret key '%s' not found" % decrypt_key_id
-        return
-    if len(decrypt_keys) != 1:
-        msg.error = "Multiple secret keys found with id '%s', be more specific" % decrypt_key_id
-        return
-
-    decrypt_key = decrypt_keys[0]
+    decrypt_key, error = get_single_key(decrypt_key_id, True)
+    if error:
+        return error
 
     # attempt decryption
     in_buf  = io.BytesIO(msg.message.encode("utf8"))
