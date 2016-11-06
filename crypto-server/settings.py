74f66f8b6591874da2d5937f6f581c7da84603b8
Dom Williams <me@domwillia.ms>
2016-11-06 16:44:56 +0100

    Fix settings order

--- crypto-server/settings.py
+++ crypto-server/settings.py
@@ -19,15 +19,15 @@ _DEFAULT_CONVO_SETTINGS = {
         "signing":    False
         }
 
-_DEFAULT_SETTINGS_FULL = {
-        "ignore-revoked": Setting("Ignore revoked keys", "Don't use revoked public keys for encryption", True),
-        "verbose-header": Setting("Show verbose message status", "Show decryption and signature status above every GPG message", True),
-        "message-colour": Setting("Enable message colours", "Indicate decryption and verification success by changing the colour of PGP messages", True),
-        "block-files": Setting("Block attachments and stickers", "Block the sending of attachments and stickers, as their encryption is not currently supported", False),
-        "numerical-hotkeys": Setting("Enable numerical hotkeys", "Enable jumping to a user's key in the participants tab with their index - e.g. to edit user #2's key, type 2 followed by Enter", True)
-        }
+_DEFAULT_SETTINGS_FULL = OrderedDict([
+    ("ignore-revoked", Setting("Ignore revoked keys", "Don't use revoked public keys for encryption", True)),
+    ("verbose-header", Setting("Show verbose message status", "Show decryption and signature status above every GPG message", True)),
+    ("message-colour", Setting("Enable message colours", "Indicate decryption and verification success by changing the colour of PGP messages", True)),
+    ("block-files", Setting("Block attachments and stickers", "Block the sending of attachments and stickers, as their encryption is not currently supported", False)),
+    ("numerical-hotkeys", Setting("Enable numerical hotkeys", "Enable jumping to a user's key in the participants tab with their index - e.g. to edit user #2's key, type 2 followed by Enter", True))
+    ])
 
-_DEFAULT_SETTINGS = {k: v.value for k, v in _DEFAULT_SETTINGS_FULL.items()}
+_DEFAULT_SETTINGS = OrderedDict([(k, v.value) for k, v in _DEFAULT_SETTINGS_FULL.items()])
 
 
 def update_convo_settings_handler(msg):
@@ -82,7 +82,7 @@ def update_settings_handler(msg):
 def get_settings():
     config.reload()
     user_set = config.get_section('settings')
-    merged = dict(_DEFAULT_SETTINGS)
+    merged = OrderedDict(_DEFAULT_SETTINGS)
     merged.update(user_set)
 
     return merged
