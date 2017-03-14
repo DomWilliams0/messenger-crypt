cad650a6ab582265b1d4a35891f5b0c4fbf78186
Dom Williams <me@domwillia.ms>
2017-03-14 12:17:28 +0100

    Add setting fetching to config context

--- native/include/config.h
+++ native/include/config.h
@@ -8,4 +8,42 @@ struct config_context;
 struct config_context *config_ctx_create();
 void config_ctx_destroy(struct config_context *ctx);
 
+enum setting_type
+{
+	SETTING_TEXT = 0,
+	SETTING_BOOL
+};
+
+struct setting_value
+{
+	union
+	{
+		const char *text;
+		int bool;
+	} value;
+};
+
+struct setting_key_instance
+{
+	const char *key;
+	const char *title;
+	const char *description;
+	enum setting_type type;
+	struct setting_value default_value;
+};
+
+enum setting_key
+{
+	SETTING_VERBOSE_HEADER = 0,
+	SETTING_IGNORE_REVOKED,
+	SETTING_MESSAGE_COLOUR,
+	SETTING_BLOCK_FILES,
+
+	SETTING_LAST
+};
+
+int config_get_setting(struct config_context *ctx, enum setting_key key, struct setting_value *out);
+// TODO config_get_conversation_settings
+// TODO config_get_key
+
 #endif
--- native/src/config.c
+++ native/src/config.c
@@ -3,10 +3,23 @@
 
 #include "config.h"
 
+#define INIT_SETTING(setting_key, key, title, desc, type, default_val) do { \
+	ctx->settings[setting_key] = (struct setting_key_instance) { \
+		key, title, desc, type, default_val \
+	}; \
+	} \
+	while (0)
+
+#define VALUE_BOOL(val) (struct setting_value) { .value = {.bool = val } }
+#define VALUE_TEXT(val) (struct setting_value) { .value = {.text = val } }
+
+
 struct config_context
 {
 	config_t config;
+	struct setting_key_instance settings[SETTING_LAST];
 };
+
 
 struct config_context *config_ctx_create()
 {
@@ -16,10 +29,41 @@ struct config_context *config_ctx_create()
 
 	config_init(&ctx->config);
 
-	if (config_read_file(&ctx->config, CONFIG_PATH) != CONFIG_TRUE)
-	{
-		// TODO load defaults?
-	}
+	// failure ignored, defaults will be used
+	config_read_file(&ctx->config, CONFIG_PATH);
+
+	INIT_SETTING(
+			SETTING_IGNORE_REVOKED,
+			"ignore-revoked",
+			"Ignore revoked keys",
+			"Don't use revoked public keys for encryption",
+			SETTING_BOOL,
+			VALUE_BOOL(1)
+			);
+	INIT_SETTING(
+			SETTING_VERBOSE_HEADER,
+			"verbose-header",
+			"Show verbose message status",
+			"Show decryption and signature status above every GPG message",
+			SETTING_BOOL,
+			VALUE_BOOL(1)
+			);
+	INIT_SETTING(
+			SETTING_MESSAGE_COLOUR,
+			"message-colour",
+			"Enable message colours",
+			"Indicate decryption and verification success by changing the colour of PGP messages",
+			SETTING_BOOL,
+			VALUE_BOOL(1)
+			);
+	INIT_SETTING(
+			SETTING_BLOCK_FILES,
+			"block-files",
+			"Block attachments and images",
+			"Block the sending of attachments and images, as their encryption is not currently supported",
+			SETTING_BOOL,
+			VALUE_BOOL(1)
+			);
 
 	return ctx;
 }
@@ -28,4 +72,34 @@ void config_ctx_destroy(struct config_context *ctx)
 {
 	config_destroy(&ctx->config);
 	free(ctx);
+}
+
+static int get(struct config_context *ctx, const char *path, enum setting_type type, struct setting_value *out)
+{
+	switch(type)
+	{
+		case SETTING_TEXT:
+			return config_lookup_string(&ctx->config, path, &out->value.text);
+		case SETTING_BOOL:
+			return config_lookup_bool(&ctx->config, path, &out->value.bool);
+		default:
+			return CONFIG_FALSE;
+	}
+}
+
+int config_get_setting(struct config_context *ctx, enum setting_key key, struct setting_value *out)
+{
+	char key_path[128] = {0};
+	struct setting_key_instance *setting = &ctx->settings[key];
+
+	if (snprintf(key_path, 128, "settings.%s", setting->key) <= 0)
+		return 1;
+
+	if (get(ctx, key_path, setting->type, out) != CONFIG_TRUE)
+	{
+		// default
+		*out = setting->default_value;
+	}
+
+	return 0;
 }
