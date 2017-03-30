75b8c4b96202c90deb85cf75ef8b7cdcf105b810
Dom Williams <me@domwillia.ms>
2017-03-30 18:30:24 +0200

    Add more protocol tests

--- native/tests/tests_protocol.py
+++ native/tests/tests_protocol.py
@@ -2,7 +2,7 @@ import struct
 import io
 import json
 
-def test_invalid(process):
+def test_raw(process):
     process.do_assert("Bad junk", "thisisnotvalid", "", send_raw=True)
     process.do_assert("Empty json", {}, "")
 
@@ -25,6 +25,20 @@ def test_invalid(process):
     bad_buf.write(struct.pack("I", ser_len * 2))
     process.do_assert("Size too big", bad_buf, "", send_raw=True)
 
+def test_json(process):
+    msg = { "what": "something or other" }
+    process.do_assert("Bad what", msg, "")
+
+    msg["what"] = 5
+    process.do_assert("Bad what type", msg, "")
+
+    msg["content"] = { 1: 2, 3: 4 }
+    process.do_assert("Bad content", msg, "")
+
+    msg["content"] = "this isnt right"
+    process.do_assert("Bad content type", msg, "")
+
 
 def run_tests(process):
-    test_invalid(process)
+    test_raw(process)
+    test_json(process)
