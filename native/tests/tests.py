221e0a8e97c12aab7466c3b5d076a56f4246bc3b
Dom Williams <me@domwillia.ms>
2017-03-30 12:00:51 +0200

    Add IPC to native testing

--- native/tests/tests.py
+++ native/tests/tests.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python2
 
 import sys
+import subprocess
+import io
+import struct
+import json
 
 # declare all test modules here
 import tests_protocol
+
+class ProcessInstance(object):
+    EXE_PATH = "../bin/messenger_crypt_native" # TODO ooer
+
+    def __init__(self):
+        try:
+            self.p = subprocess.Popen(self.EXE_PATH, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        except OSError as e:
+            sys.stderr.write("Failed to start process: %s\n" % e)
+            sys.exit(1)
+
+    # req is json
+    def send_request(self, req):
+        raw = json.dumps(req)
+        out = io.BytesIO()
+        out.write(struct.pack("I", len(raw)))
+        out.write(raw)
+        out.flush()
+
+        return self._send(out)
+
+    # req is a string
+    def send_raw_request(self, req):
+        return self._send(io.BytesIO(req))
+
+    # req is a filelike object
+    def _send(self, req):
+        req.seek(0)
+        resp = self.p.communicate(req.read())
+        return resp[0]
+
 
 def _get_tests(what):
     import types
@@ -24,6 +59,8 @@ def run_tests(what):
         sys.stderr.write("No test suites found\n");
         sys.exit(1)
 
+    instance = ProcessInstance()
+
     pass_count = 0
     for t in tests:
         name = t.__name__[len("tests_"):]
@@ -34,7 +71,7 @@ def run_tests(what):
             continue
 
         sys.stdout.write("----- BEGIN TEST SUITE '%s'\n" % name)
-        success = entry()
+        success = entry(instance)
 
         if success is True:
             pass_count += 1
